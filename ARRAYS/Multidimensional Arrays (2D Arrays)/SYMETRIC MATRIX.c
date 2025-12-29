500d33f254ce8aae866813d9a4329e9f8fd09b3e
prajwaluvirup29-collab <prajwaluvirup29@gmail.com>
2025-12-29 20:13:46 +0530

    MULTIDIMENSIONAL ARRAYS
    MULTIDIMENSIONAL ARRAYS

---
+++ ARRAYS/Multidimensional Arrays (2D Arrays)/SYMETRIC MATRIX.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j, flag = 1;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    if(r != c) {
+        printf("Matrix is not symmetric\n");
+        return 0;
+    }
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            if(a[i][j] != a[j][i]) {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("Matrix is symmetric\n");
+    else
+        printf("Matrix is not symmetric\n");
+
+    return 0;
+}
